d88edda69e218272733b7b0b324acf70fbd90794
Christian Klinger <cklinger@novareto.de>
2013-09-02 15:59:08 +0200

    more tests

--- src/nva/mq/test_receiver.py
+++ src/nva/mq/test_receiver.py
@@ -9,10 +9,40 @@ from cStringIO import StringIO
 from zope.component import globalregistry as gr
 from ZODB import DB
 from ZODB.DemoStorage import DemoStorage
-        
+
 TEST_URL = "memory://localhost:8888//"
 
 from nva.mq.test_manager import ZCML
+from cromlech.zodb.controlled import Connection as ZODBConnection
+from kombu import Connection as AMQPConnection
+from kombu.common import drain_consumer
+
+
+RECEIVED = []
+
+
+class TestWorker(Worker):
+
+    def process(self, body, message):
+        super(TestWorker, self).process(body, message)
+        RECEIVED.append(message)
+
+
+class TestBaseReader(BaseReader):
+
+    def start(self, url, db, appname, queues):
+        print "--Starting %s--" % self
+        tm = transaction.TransactionManager()
+        queues = [getUtility(IQueue, name=queue) for queue in queues]
+        with ZODBConnection(db, transaction_manager=tm) as zodb:
+            with tm:
+                #root = zodb.root()[ZopePublication.root_name]
+                #site = root[appname]
+                site = None
+                with AMQPConnection(url) as conn:
+                    consumer = TestWorker(conn, queues, tm, site)
+                    drain_consumer(consumer, limit=1, timeout=1, callbacks=[consumer.process])
+
 
 
 class MQReceiverTests(unittest.TestCase):
@@ -20,11 +50,11 @@ class MQReceiverTests(unittest.TestCase):
     def setUp(self):
         gr.globalSiteManager = gr.BaseGlobalComponents('test2')
         xmlconfig(StringIO(ZCML))
-        self.receiver = BaseReader()
+        self.receiver = TestBaseReader()
         queues = [getUtility(IQueue, name=queue) for queue in ['info', 'error']]
         self.dm = MQDataManager(url=TEST_URL, queues=queues)
         self.db = DB(DemoStorage('test_storage'))
-        
+
     def tearDown(self):
         gr.globalSiteManager = gr.base
 
@@ -34,3 +64,4 @@ class MQReceiverTests(unittest.TestCase):
             self.dm.createMessage(Message('info', data={'message': "INFO"}))
             self.dm.createMessage(Message('error', data={'message': "ERROR"}))
         self.receiver.start(TEST_URL, self.db, 'app', ['info', 'error'])
+        assert len(RECEIVED) == 2
