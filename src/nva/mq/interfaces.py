54da31dff8e53eea4e5ab7c187fb1807791f2562
goschtl <ck@novareto.de>
2013-09-04 14:13:27 +0200

    chckpoint

--- src/nva/mq/interfaces.py
+++ src/nva/mq/interfaces.py
@@ -2,8 +2,9 @@
 # Copyright (c) 2007-2013 NovaReto GmbH
 # cklinger@novareto.de
 
-
 from zope.component import Interface
+from zope.component.interfaces import IFactory
+
 
 class ISender(Interface):
 
@@ -13,11 +14,27 @@ class ISender(Interface):
 
 class IReceiver(Interface):
 
-    def start(url, db, appname, queues):
-        pass
-
+    def receive(queue_ids, limit=None, timeout=None, **data):
+        """Receive the messages on the queues defined by `queue_ids`
+        `limit` : number of message to receive before quitting
+        `timeout`: timeout before dsconnection
+        `data`: dict-like datastructure passed to the task processor.
+        """
+        
 
 class IProcessor(Interface):
 
-    def __call__(data):
+    def __call__(body, message, **data):
         pass
+
+
+
+class IListener(IFactory):
+
+    def __call__(url):
+        """Returns an instance of an IReicever for the given url.
+        """
+
+
+
+    
--- src/nva/mq/runner.py
+++ src/nva/mq/runner.py
@@ -5,91 +5,96 @@
 import os
 import argparse
 import transaction
+import socket
+from kombu.utils import nested
 from cromlech.configuration.utils import load_zcml
-from cromlech.zodb.utils import init_db
-from nva.mq.interfaces import IReceiver, IProcessor
-from nva.mq.queue import IQueue
-from zope.component import getUtility
-from grokcore.component import global_utility
-from zope.interface import Interface
-from zope.app.publication.zopepublication import ZopePublication
 from cromlech.zodb.controlled import Connection as ZODBConnection
-from kombu import Connection as AMQPConnection
-from kombu.mixins import ConsumerMixin
-from kombu.common import drain_consumer
-
-parser = argparse.ArgumentParser(usage="usage: prog [options]")
-
-parser.add_argument(
-    '--zodb_conf', dest="zodb_conf", default=None,
-    help=u'ZODB configuration file')
-
-parser.add_argument(
-    '--zcml', dest="zcml", default=None,
-    help=u'ZCML configuration file')
-
-parser.add_argument(
-    '--app', dest="app", default='app',
-    help=u'App Name')
-
-parser.add_argument(
-    '--url', dest="url", default='amqp://guest:quest@locahost//',
-    help=u'URL of the AMQP server')
+from cromlech.zodb.utils import init_db
+from grokcore.component import global_utility
+from kombu import Consumer, Connection as AMQPConnection
+from kombu.common import drain_consumer, itermessages
+from nva.mq.interfaces import IListener, IReceiver, IProcessor
+from nva.mq.manager import Message, MQTransaction
+from nva.mq.queue import IQueue
+from zope.app.publication.zopepublication import ZopePublication
+from zope.component import getUtility
+from zope.interface import Interface
 
 
-class Worker(ConsumerMixin):
+def resolve_queues(qids):
+    for qid in qids:
+        try:
+            queue = getUtility(IQueue, name=qid)
+            yield qid, queue
+        except:
+            # handle me
+            raise
 
-    def __init__(self, connection, queues, tm, site):
-        self.connection = connection
-        self.site = site
-        self.queues = queues
-        self.tm = tm
 
-    def get_consumers(self, Consumer, channel):
-        return [Consumer(queues=self.queues, callbacks=[self.process])]
+def processor(name, **data):
+    handler = getUtility(IProcessor, name=name)
+    def process(body, message):
+        errors = handler(body, message, **data)
+        if errors is None:
+            message.ack()
+    return process
 
-    def process(self, body, message):
-        processor = body.get('processor')
-        if processor is not None:
-            handler = getUtility(IProcessor, name=processor)
-            handler(body)
-        else:
-            print body
-        message.ack()
 
 
 class BaseReader(object):
 
-    worker = Worker
+    def __init__(self, url):
+        self.url = url
 
-    def start(self, url, db, appname, queues):
-        print "--Starting %s--" % self
-        tm = transaction.TransactionManager()
-        queues = [getUtility(IQueue, name=queue) for queue in queues]
-        with ZODBConnection(db, transaction_manager=tm) as zodb:
-            with tm:
-                #root = zodb.root()[ZopePublication.root_name]
-                #site = root[appname]
-                site = None
-                with AMQPConnection(url) as conn:
-                    consumer = Worker(conn, queues, tm, site)
-                    drain_consumer(consumer, limit=1, timeout=1)
+    def poll(self, qids, limit=None, timeout=None, **data):
+        print "--Starting the poller for %s, %s--" % (qids, data)
+        queues = resolve_queues(qids)
+        with AMQPConnection(self.url) as conn:
+            consumers = [Consumer(conn.channel(), queues=[queue],
+                                  callbacks=[processor(name, **data)])
+                for name, queue in queues]
+            with nested(*consumers):
+                while True:
+                    try:
+                        conn.drain_events(timeout=timeout)
+                    except socket.timeout:
+                        break
 
-global_utility(BaseReader, provides=IReceiver)
-
-
-def init(name, conf_file):
-    with open(conf_file, 'r') as fs:
-        config = fs.read()
-    db = init_db(config)
-    return db
+                    
+global_utility(BaseReader, provides=IListener, direct=True)
 
 
-def main(zodb_conf, app, url, zcml=None):
+def info_processor(body, message, **data):
+    print body, message, data
 
-    if zcml:
-        load_zcml(zcml)
 
-    db = init(app, zodb_conf)
-    receiver = getUtility(IReceiver)
-    receiver.start(url, db, app, ['info'])
+global_utility(info_processor, provides=IProcessor, direct=True, name="info")
+    
+
+def poller(zodb_conf=None, app="app", url=None, zcml_file=None):
+
+    if zcml_file:
+        load_zcml(zcml_file)
+
+    db = init_db(zodb_conf)
+    
+    listener = getUtility(IListener)
+    receiver = listener(url)
+
+    tm = transaction.TransactionManager()
+    with ZODBConnection(db, transaction_manager=tm) as zodb:
+        with tm:
+           receiver.poll(['info'], timeout=2, **{'db_root': db})
+
+
+def sender(url=None, zcml_file=None):
+    """Used for test purposes.
+    """
+    if zcml_file:
+        load_zcml(zcml_file)
+
+    message = Message('info', **{'message': 'ping'})
+    queue = getUtility(IQueue, name='info')
+    with transaction.manager as tm:
+        with MQTransaction(url, [queue], tm) as message_manager:
+            message_manager.createMessage(message)
